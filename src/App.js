5632f56bac2052324cbee9b07215e06206957087
beshiralkhadra <beshiralkhadra@gmail.com>
2021-12-02 23:04:23 +0200

    My Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 const App = () => {
   // All State
-  return <div className="App">Hello Team . enbbbbbbbbbb</div>;
+  return <div className="App">Hello Team . enbbbbbbbbb</div>;
 };
 
 export default App;
