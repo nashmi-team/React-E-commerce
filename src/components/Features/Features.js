6d9ee0d0851409e614e67f1df6232e131cfb3a3e
DeekCS <31720001079@std.bau.edu.jo>
2021-12-05 00:30:45 +0200

    Styling Features and Product Slider

---
+++ src/components/Features/Features.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './features.css';
+// <i className="fas fa-sync"></i>
+
+const features = [
+    {
+        title: 'Free Shipping',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisci',
+        icon: 'fas fa-truck'
+    }
+    ,
+    {
+        title: 'Return Policy',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisci',
+        icon: 'fas fa-sync'
+    }
+    ,
+    {
+        title: '24/7 Support\n',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisci',
+        icon: 'fas fa-headset'
+    },
+    {
+        title: 'Secure Payment',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisci',
+        icon: 'fas fa-money-check-alt'
+    }
+]
+const Features = () => {
+    return (
+        <section className="features-area section_gap py-5">
+            <div className="container">
+                <div className="features-row">
+                    {features.map((feature, index) => (
+                        <div className="single-features" key={index}>
+                            <span className="features-icon">
+                                <i className={feature.icon} />
+                            </span>
+                            <div className="desc">
+                                <h6 className="title-feature">{feature.title}</h6>
+                                <p>{feature.text}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                </div>
+        </section>
+    );
+};
+
+export default Features;
+/*
+  <div className="single-features">
+                        <span className="features-icon">
+                            <i className="fas fa-truck"/>
+                        </span>
+                        <h6 className="title-feature">Easy to use</h6>
+                        <p>
+                            inappropriate behavior is often laughed off as “
+                        </p>
+                    </div>
+ */
