f9a12e724fe599fd89e60593adc9615c0557529d
ahmadaldabouqii <aldabouqiahmad@gmail.com>
2021-12-05 16:23:52 +0200

    my-work ahmad

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import SingleProduct from "./components/Shop/SingleProduc/SingleProduct";
 
 const App = () => {
   // All State
-  const [updatedData, setUpdatedData] = useState({ items: "" });
   const [userSignupInformation, setUserSignupInformation] = useState({
     username: "",
     email: "",
@@ -29,30 +28,6 @@ const App = () => {
 
   const [submitted, setSubmitted] = useState(false);
   const [logged, setLogged] = useState(false);
-
-  // Weather Data & State
-  const [dataWeather, setDataWeather] = useState({});
-
-  const get = async () => {
-    const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Amman&units=metric&appid=0cef1dfd8f5e8437bbb45886a733a63c"
-    );
-    const data = await response.json();
-
-    setDataWeather({
-      name: data.name,
-      temp: Math.round(data.main.temp),
-      description: data.weather[0].description,
-      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
-    });
-  };
-  useEffect(() => get(), []);
-
-  const local = () => {
-    const localData = JSON.parse(localStorage.getItem("react-use-cart"));
-    setUpdatedData({ items: localData.items });
-  };
-  useEffect(() => local(), []);
 
   return (
     <Router>
@@ -82,12 +57,7 @@ const App = () => {
             <Shop />
           </Route>
           <Route path="/dataWeather">
-            <Profile
-              dataWeather={dataWeather}
-              logged={logged}
-              setLogged={setLogged}
-              updatedData={updatedData}
-            />
+            <Profile logged={logged} setLogged={setLogged} />
           </Route>
           <Route exact path="/Product/:id" children={<Shop />}>
             <SingleProduct />
